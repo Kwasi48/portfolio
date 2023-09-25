4f2882c3198fbff78ac2e385091b95c7c8180ff9
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-09-25 12:27:52 +0000

    Removed the Stack. About to restrategize and design the profile all again

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -6,30 +6,8 @@ class profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            const Positioned(
-                top: 40,
-                left: 50,
-                child: Text(
-                  'Hi, I am Micky, a software Engineer Specialising in Flutter',
-                  style: TextStyle(fontSize: 30),
-                )),
-            const Positioned(
-                top: 90,
-                left: 50,
-                child: SizedBox(
-                  height: 100,
-                  width: 100,
-                  child: Text(
-                    'Lorem',
-                    style: TextStyle(fontSize: 25),
-                  ),
-                )),
-            Positioned(left: 50, top: 200, child: _imageCard())
-          ],
-        ),
+      body: SingleChildScrollView(
+
       ),
     );
   }
@@ -40,7 +18,7 @@ Widget _imageCard() {
     children: [
       Image(image: AssetImage('assets/mike.png',), height: 300,),
       SizedBox(width: 18,),
-      Text('Hit me up when You need to solve a problem using code!!')
+      Text('Hit me up when You need to solve a problem using code!!', style: TextStyle(fontSize: 30),)
     ],
   );
 }
