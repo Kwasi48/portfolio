bd3ab3e47c0c35540bfd3104f6887f7968c2c51c
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2023-10-02 13:13:58 +0000

    Added social media buttons to the home screen

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_buttons/social_media_buttons.dart';
 
 class profile extends StatelessWidget {
   const profile({super.key});
@@ -7,7 +8,28 @@ class profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                color: Colors.purple,
+                elevation: 10,
+                child: _imageCard(),
+              ),
+            ),
+             Row(
+               mainAxisSize: MainAxisSize.min,
+               children: [
+                 githubButton(),
+                 xButton(),
+                 linkedInButton()
+               ],
+             )
+          ],
+        ),
       ),
     );
   }
@@ -16,9 +38,41 @@ class profile extends StatelessWidget {
 Widget _imageCard() {
   return const Row(
     children: [
-      Image(image: AssetImage('assets/mike.png',), height: 300,),
-      SizedBox(width: 18,),
-      Text('Hit me up when You need to solve a problem using code!!', style: TextStyle(fontSize: 30),)
+      Image(
+        image: AssetImage(
+          'assets/mike.png',
+        ),
+        height: 300,
+      ),
+      SizedBox(
+        width: 18,
+      ),
+      Flexible(
+          child: Text(
+        'Hit me up when You need to solve a problem using code!!',
+        style: TextStyle(fontSize: 50),
+      ))
     ],
+  );
+}
+
+
+Widget githubButton() {
+  return const SocialMediaButton.github(
+    url: "https://github.com/Kwasi48",
+    color: Colors.white,
+  );
+}
+Widget xButton() {
+  return const  SocialMediaButton.twitter(
+    url: "https://twitter.com/abubonsrah47",
+    color: Colors.white,
+  );
+}
+
+Widget linkedInButton() {
+  return const  SocialMediaButton.linkedin(
+    url: "https://www.linkedin.com/in/michael-obeng-abu-08483120b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BUAcdWAogRWKB1cPZxUusVA%3D%3D",
+    color: Colors.white,
   );
 }
